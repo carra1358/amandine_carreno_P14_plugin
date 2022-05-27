2856e3fd9916323d8d7b07ea3f2276009eb0b2c3
amandine carreno <30933638+carra1358@users.noreply.github.com>
2022-05-27 15:30:46 +0200

    plugin init

---
+++ plugin/src/stories/Modal.jsx
@@ -0,0 +1,15 @@
+// import { IoCloseSharp } from "react-icons/io5"
+import React from "react";
+
+export const Modal = ({ }) => {
+
+    return (
+        <div>
+            modal
+        </div>
+    )
+
+
+}
+
+export default Modal;
---
+++ plugin/src/stories/Modal.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Modal } from "./Modal"
+
+export default {
+    title: "Component/Modal",
+    component: Modal
+}
+
+export const Template = (args) => <Modal {...args} />
+
+
